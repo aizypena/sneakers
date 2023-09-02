265eae3dd4ff4d1e0e132db008d1bbc2b28083cf
JBPena <jbpena101@gmail.com>
2023-09-03 00:04:55 +0800

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,103 +1,37 @@
-import "./Home.css";
-import { useState } from "react";
+import {useState} from "react";
+import "./Home.css"
 
 function Home() {
-    //to display image
-    const initialImage = "./p1.jpg";
-
-    const [displayedImage, setDisplayImage] = useState(initialImage);
-
-    const handleImageClick = (imageUrl) => {
-        setDisplayImage(imageUrl);
-    };
-
-    //buttons
-    const [counter, setCount] = useState(0);
-
-    const minus = () => {
-        if (counter > 0) {
-            setCount(counter - 1);
-        }
-    };
-
-    const add = () => {
-        setCount(counter + 1);
-    };
-
     return (
-        <div className="container home-main">
-            <div className="home-left flex flex-column justify-center items-center">
-                <div id="displayImage" className="mb-4">
-                    {displayedImage && (
-                        <img
-                            src={displayedImage}
-                            alt="Displayed"
-                            className="displayedImg"
-                        />
-                    )}
-                </div>
-                <div className="flex justify-between items-center picsContainer">
-                    <img
-                        src="./p1.jpg"
-                        className="pics img-fluid"
-                        onClick={() => handleImageClick("./p1.jpg")}
-                    />
-                    <img
-                        src="./p2.jpg"
-                        className="pics img-fluid"
-                        onClick={() => handleImageClick("./p2.jpg")}
-                    />
-                    <img
-                        src="./p3.jpg"
-                        className="pics img-fluid"
-                        onClick={() => handleImageClick("./p3.jpg")}
-                    />
-                    <img
-                        src="./p4.jpg"
-                        className="pics img-fluid"
-                        onClick={() => handleImageClick("./p4.jpg")}
-                    />
-                </div>
-            </div>
-            <div className="home-right">
-                <div className="container product-deets">
-                    <p className="sneaker-txt">SNEAKER COMPANY</p>
-                    <h1 className="limited-ed">
-                        Fall limited edition sneakers
-                    </h1>
-                    <p className="sneaker-description">
-                        These low-profile sneakers are your perfect casual wear
-                        companion. Featuring a durable rubber outer sole,
-                        they'll withstand everything the weather can offer.
-                    </p>
-                    <div className="price-cont">
-                        <div>
-                            <h3>$150.00</h3>
-                        </div>
-                        <div className="percentage flex items-center">
-                            <p className="perc">50%</p>
-                        </div>
-                    </div>
-                    <h6 className="orig-price">$300.00</h6>
-                    <div className="flex all-btn">
-                        <div className="flex btn-control">
-                            <button className="minus-btn" onClick={minus}>
-                                <p className="marg-0 btn-1 minus-p">-</p>
-                            </button>
-                            <p className="counter-output marg-0 btn-1">
-                                {counter}
-                            </p>
-                            <button className="add-btn" onClick={add}>
-                                <p className="marg-0 btn-1 add-p">+</p>
-                            </button>
-                        </div>
-                        <div className="cart-btn-cont">
-                            <button className="add2cart">Add to cart</button>
-                        </div>
+        <>
+            <div className="home-main">
+                <div className={"container home-container"}>
+                    <div className={"cont"}>
+
+                        {/*first section of my page*/}
+                        <section className={"pics-desc"}>
+                            <div className={"pics-highlight"}>
+                                <div className={" pic-btn"}>
+                                    <p className={"back pic-btn-p"}>&lt;</p>
+                                </div>
+                                <div className={" pic-btn"}>
+                                    <p className={"next pic-btn-p"}>&gt;</p>
+                                </div>
+                            </div>
+                            <div className={"product-pics"}>
+                                <img className={"img-fluid"} src={"./p1.jpg"}/>
+                                <img className={"img-fluid"} src={"./p2.jpg"}/>
+                                <img className={"img-fluid"} src={"./p3.jpg"}/>
+                                <img className={"img-fluid"} src={"./p4.jpg"}/>
+                            </div>
+                        </section>
+
+                        {/*second section*/}
+                        <section className={"btns"}></section>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
